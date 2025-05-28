816c8c384ef5301d9d297a7e57321cf6cb6cb2a9
beazinat <beatrizgarodrigues@gmail.com>
2025-05-28 11:28:05 -0300

    :recycle: refactor: Refactoring exercise 4

--- LinguagensDeProgramaçãoIII/ex_4.c
+++ LinguagensDeProgramaçãoIII/ex_4.c
@@ -4,6 +4,10 @@ Exercício 4 - As maçãs custam R$1,50 cada se menos de uma dúzia for comprada
 
 #include <stdio.h>
 
+/*
+Esse algoritmo lê a quantidade de maçãs compradas pelo usuário e calcula o total a pagar com base no preço unitário. Se a quantidade for menor que 12, o preço é R$1,50 por maçã; caso contrário, o preço é R$1,00 por maçã. O programa também informa se o usuário ganhou um desconto especial e quanto economizou.
+*/
+
 int main() {
     int quantidade;
     float preco;
@@ -11,13 +15,19 @@ int main() {
     printf("Digite a quantidade de maçãs compradas: ");
     scanf("%d", &quantidade);
 
-    if (quantidade < 12) {
-        preco = quantidade * 1.50;
-    } else {
-        preco = quantidade * 1.00;
+    if (quantidade < 0) {
+        printf("Quantidade inválida. Digite um valor positivo.\n");
+        return 1;
     }
 
-    printf("Total a pagar: R$%.2f\n", preco);
+    if (quantidade < 12) {
+        preco = quantidade * 1.50;
+        printf("Total a pagar: R$%.2f\n", preco);
+    } else {
+        preco = quantidade * 1.00;
+        printf("Parabéns, você ganhou o desconto especial! Total a pagar: R$%.2f\n", preco);
+        printf("Quanto você economizou: R$%.2f\n", (quantidade * 1.50) - preco);
+    }
 
     return 0;
 }
